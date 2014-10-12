12279bd70e33a3c070e0e5424d816f457de8a703
Ryo Hashimoto <ryohashimoto@gmail.com>
2014-10-13 02:36:04 +0900

    フォームの追加

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,33 @@
 package controllers;
 
 import play.*;
+import play.data.*;
+import static play.data.Form.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("これは最初の値です。",
-			       "これは真ん中の引数です。",
-			       "これは最後のテキストです。"));
+    // Form用の内部クラス
+    public static class SampleForm {
+	public String message;
     }
 
+    // ルートにアクセスした際のAction
+    public static Result index() {
+	return ok(index.render("何か書いて。", new Form(SampleForm.class)));
+    }
+
+    // /sendにアクセスした際のAction
+    public static Result send() {
+	Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
+	if (!f.hasErrors()) {
+	    SampleForm data = f.get();
+	    String msg = "you typed: " + data.message;
+	    return ok(index.render(msg, f));
+	} else {
+	    return badRequest(index.render("ERROR", form(SampleForm.class)));
+	}
+    }
 }
